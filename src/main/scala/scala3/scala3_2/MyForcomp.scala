b1a9bb51b6beb71aafb8d67ae1b5f1d3530ed22d
maksim <corystov.maks@yandex.ru>
2024-09-08 21:35:24 +0300

    CHANGE Lesson3 Scala3 MyForcomp

--- src/main/scala/scala3/scala3_2/MyForcomp.scala
+++ src/main/scala/scala3/scala3_2/MyForcomp.scala
@@ -48,8 +48,63 @@ object MyForcomp {
     ){
       println(s"$el1 $el2 $el3")
     }
-  }
 
+    println("-------------------------")
+    println("example")
+
+    // создадим класс машин
+    case class Car(marke: String, model: String, year: Int)
+
+    // создадим коллекцию машин
+    val cars = Car("VW", "Passat", 2005) :: Car("Lexus", "Ux", 2019) :: Car("BMW", "ix4", 2023) :: Nil
+
+    // создадим класс гаражей
+    case class Garage(name: String, index: Int)
+    val garages = Garage("BMW", 1) :: Garage("Lexus", 2) :: Nil
+
+    garages.flatMap{
+      garage =>
+        cars.filter(car => car.marke == garage.name).map(car => (car.marke, car.model, garage.index))
+    }.foreach{case (marke, garage, index) => println(s"$marke, $garage $index")}
+    /*
+      BMW, ix4 1
+      Lexus, Ux 2
+     */
+
+    println("-------------------------")
+
+    println("for comp.")
+
+    val cars2000 = for {
+      car <- cars if car.year > 2010
+    } yield (car.marke, car.model)
+
+    cars2000.foreach(x => println(s"${x._1} ${x._2}"))
+
+    println("-------------------------")
+
+    val car1 = for {
+      car <- cars       // cars = Car("VW", "Passat", 2005) :: Car("Lexus", "Ux", 2019) :: Car("BMW", "ix4", 2023) :: Nil
+      garage <- garages // garages = Garage("BMW", 1) :: Garage("Lexus", 2) :: Nil
+      if car.marke == garage.name
+    } yield {
+      (car.marke, car.model, garage.index)
+    }
+
+    car1.foreach({
+      case (marke, model, index) => println(s"$marke, $model, $index")  // case дает имена переменным
+    })
+    /*
+      Lexus, Ux, 2
+      BMW, ix4, 1
+     */
+    
+    // << Законы Монады >>
+    
+    // 1. unit(a).flatMap(f) == f(a)
+    // 2. ma.flatMap(unit) == ma
+    // 3. ma.flatMap(f).flatMap(g) == ma.flatMap(x => f(x).flatMap(g))
+  }
 }
 
 
